1ac5294eaed8188e721d25b9447327b1668086c7
Xu Chenglin PhD MICL <clx214@scsegc.cm.cluster>
2020-06-24 18:07:11 +0800

    format some code

--- train.sh
+++ train.sh
@@ -5,11 +5,9 @@ set -eu
 cpt_dir=exp/conv_tasnet
 epochs=100
 # constrainted by GPU number & memory
-batch_size=4
-batch_size=2
+batch_size=20
 cache_size=16
 
 #[ $# -ne 2 ] && echo "Script error: $0 <gpuid> <cpt-id>" && exit 1
 
-./nnet/train.py --gpu "1,2" --epochs $epochs --batch-size $batch_size --checkpoint $cpt_dir/conv-net 
-./nnet/train.py --gpu "3" --epochs $epochs --batch-size $batch_size --checkpoint $cpt_dir/conv-net 
+./nnet/train.py --gpu "0,1,2" --epochs $epochs --batch-size $batch_size --checkpoint $cpt_dir/conv-net 
